7755f4a2190a9fdcdc59f3e01a55a09b7294dc61
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2025-04-13 01:21:37 +0200

    Kinda refactorings

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "2.1.10"
 }
 
 group = "org.jetbrains"
@@ -10,7 +10,9 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":"))
+    api(project(":"))
+    compileOnly("org.jetbrains.kotlinx:kotlin-jupyter-api:0.12.0-398")
+    compileOnly("org.jetbrains.kotlinx:kotlin-jupyter-lib:0.12.0-398")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
